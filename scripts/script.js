50f2a0217200d63a60c4cfa3600fdc9d15226aae
Sarah Liu <sarahliu1998@gmail.com>
2023-02-28 13:14:39 -0800

    added main.css and edited script.js

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,19 @@
-function sayHello() {
-    
+var map = L.map('map').setView([49.250756, -123.0007691], 17);
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
+
+var marker = L.marker([49.250756, -123.0007691]).addTo(map);
+marker.bindPopup("<b>BCIT</b><br>British Columbia Institute of Technology").openPopup();
+
+var popup = L.popup();
+
+function onMapClick(e) {
+  popup
+    .setsLatLng(e.latlng)
+    .setContent("You clicked the map at " + e.latlng.toString())
+    .openOn(map);
 }
-//sayHello();
+map.on('click', onMapClick);
