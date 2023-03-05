9a0ad135648f5d5d7202de7909f212cdc0d49c6c
Sarah Liu <sarahliu1998@gmail.com>
2023-03-04 17:38:40 -0800

    added posts page and separate js for map

---
+++ scripts/map.js
@@ -0,0 +1,19 @@
+var map = L.map('map').setView([49.250756, -123.0007691], 17);
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
+
+var marker = L.marker([49.250756, -123.0007691]).addTo(map);
+marker.bindPopup("<b>BCIT</b><br>British Columbia Institute of Technology").openPopup();
+
+var popup = L.popup();
+
+function onMapClick(e) {
+    popup
+        .setLatLng(e.latlng)
+        .setContent("You clicked the map at " + e.latlng.toString())
+        .openOn(map);
+}
+map.on('click', onMapClick);
