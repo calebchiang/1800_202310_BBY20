8c9aa806dec01a53226b1438814b760d89595d83
Josh Hipkin <j.hipkin4@gmail.com>
2023-03-10 12:58:38 -0800

    Josh made some changes to styling

--- scripts/upload_2.js
+++ scripts/upload_2.js
@@ -1,65 +1,69 @@
 var ImageFile;
 function listenFileSelect() {
-      // listen for file selection
-      var fileInput = document.getElementById("photo"); // pointer #1
-      const image = document.getElementById("image"); // pointer #2
+  var fileInput = document.getElementById("photo");
+  const image = document.getElementById("image");
 
-			// When a change happens to the File Chooser Input
-      fileInput.addEventListener('change', function (e) {
-          ImageFile = e.target.files[0];   //Global variable
-          var blob = URL.createObjectURL(ImageFile);
-          image.src = blob; // Display this image
-      })
+  fileInput.addEventListener("change", function (e) {
+    ImageFile = e.target.files[0];
+    var blob = URL.createObjectURL(ImageFile);
+    image.src = blob;
+  });
 }
 listenFileSelect();
 
-        function uploadPost() {
-       alert ("SAVE POST is triggered");
-       firebase.auth().onAuthStateChanged(function (user) {
-           if (user) {
-               // User is signed in.
-               // Do something for the user here. 
-               var desc = document.getElementById("hazard-description").value;
-               var severity = document.getElementById("severity-select").value;
-               db.collection("posts").add({
-                   owner: user.uid,
-                   image: "",
-                   description: desc,
-                   severity: severity,
-                   last_updated: firebase.firestore.FieldValue
-                       .serverTimestamp() //current system time
-               }).then(doc => {
-                   console.log("Post document added!");
-                   console.log(doc.id);
-                   uploadPic(doc.id);
-               })
-           } else {
-               // No user is signed in.
-							 console.log("Error, no user signed in");
-           }
-       });
+document.addEventListener("keypress", function (event) {
+  if (event.key === "Enter") {
+    // event.preventDefault();
+    document.getElementById("submit-button").click();
+  }
+});
+function uploadPost() {
+  alert("Post is being uploaded");
+  firebase.auth().onAuthStateChanged(function (user) {
+    if (user) {
+      var desc = document.getElementById("hazard-description").value;
+      var severity = document.getElementById("severity-select").value;
+      db.collection("posts")
+        .add({
+          owner: user.uid,
+          image: "",
+          description: desc,
+          severity: severity,
+          last_updated: firebase.firestore.FieldValue.serverTimestamp(), //current system time
+        })
+        .then((doc) => {
+          console.log("Post document added!");
+          console.log(doc.id);
+          uploadPic(doc.id);
+        });
+    } else {
+      // No user is signed in.
+      console.log("Error, no user signed in");
+    }
+  });
 }
 
-
 function uploadPic(postDocID) {
-    console.log("inside uploadPic " + postDocID);
-    var storageRef = storage.ref("images/" + postDocID + ".jpg");
+  console.log("inside uploadPic " + postDocID);
+  var storageRef = storage.ref("images/" + postDocID + ".jpg");
 
-    storageRef.put(ImageFile)   //global variable ImageFile
-        .then(function () {
-            console.log('Uploaded to Cloud Storage.');
-            storageRef.getDownloadURL()
-                .then(function (url) { // Get URL of the uploaded file
-                    console.log("Got the download URL.");
-                    db.collection("posts").doc(postDocID).update({
-                            "image": url // Save the URL into users collection
-                        })
-                        .then(function () {
-                            console.log('Added pic URL to Firestore.');
-                        })
-                })
-        })
-        .catch((error) => {
-             console.log("error uploading to cloud storage");
-        })
+  storageRef
+    .put(ImageFile)
+    .then(function () {
+      console.log("Uploaded to Cloud Storage.");
+      storageRef.getDownloadURL().then(function (url) {
+        console.log("Got the download URL.");
+        db.collection("posts")
+          .doc(postDocID)
+          .update({
+            image: url,
+          })
+          .then(function () {
+            console.log("Added pic URL to Firestore.");
+          });
+      });
+    })
+    .catch((error) => {
+      console.log("error uploading to cloud storage");
+    });
 }
