0c89b2dbf3ab35f2ddd147a1f6d4a6fd67457531
Josh Hipkin <j.hipkin4@gmail.com>
2023-03-27 12:17:30 -0700

    added confirm window to logout button

--- scripts/script.js
+++ scripts/script.js
@@ -2,10 +2,13 @@
 // Call this function when the "logout" button is clicked
 //-------------------------------------------------
 function logout() {
+  confirm("Are you sure you want to log out?")
+  if (confirm == true) {
     firebase.auth().signOut().then(() => {
-        // Sign-out successful.
-        console.log("logging out user");
-      }).catch((error) => {
-        // An error happened.
-      });
+      // Sign-out successful.
+      console.log("logging out user");
+    }).catch((error) => {
+      // An error happened.
+    });
+  }
 }
